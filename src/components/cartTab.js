bf5c104435e9cb682d12adcdc283ea1024696864
karantec <karanrana3095@gmail.com>
2024-08-13 19:26:35 +0530

    codecommit

--- src/components/cartTab.js
+++ src/components/cartTab.js
@@ -1,16 +1,16 @@
-import React, { useState } from 'react'
-import { useSelector, useDispatch } from 'react-redux'
-import { useNavigate } from 'react-router-dom'
-import CartItem from './cartItem';
+import React, { useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import CartItem from "./cartItem";
 import { toggleStatusTab, applyDiscount, clearCart } from '../stores/cart';
 import { formatCurrency } from '../utils/currencyFormatter';
 
 const CartTab = () => {
-    const carts = useSelector(store => store.cart.items);
-    const statusTab = useSelector(store => store.cart.statusTab);
-    const totalBalance = useSelector(store => store.cart.totalBalance);
-    const discount = useSelector(store => store.cart.discount);
-    const finalPrice = useSelector(store => store.cart.finalPrice);
+    const carts = useSelector((store) => store.cart.items);
+    const statusTab = useSelector((store) => store.cart.statusTab);
+    const totalBalance = useSelector((store) => store.cart.totalBalance);
+    const discount = useSelector((store) => store.cart.discount);
+    const finalPrice = useSelector((store) => store.cart.finalPrice);
     const dispatch = useDispatch();
     const navigate = useNavigate();
     const [discountInput, setDiscountInput] = useState('');
@@ -18,34 +18,39 @@ const CartTab = () => {
 
     const handleCloseTabCart = () => {
         dispatch(toggleStatusTab());
-    }
+    };
 
     const handleApplyDiscount = () => {
         const discountValue = parseFloat(discountInput);
         if (!isNaN(discountValue) && discountValue > 0) {
-            dispatch(applyDiscount({ 
-                discountAmount: discountValue, 
-                isPercentage: discountType === 'percentage' 
-            }));
+            dispatch(
+                applyDiscount({
+                    discountAmount: discountValue,
+                    isPercentage: discountType === 'percentage',
+                })
+            );
         }
-    }
+    };
 
     const handleCheckout = () => {
         dispatch(clearCart());
         dispatch(toggleStatusTab());
         navigate('/thank-you', { state: { totalAmount: finalPrice } });
-    }
+    };
 
     return (
-        <div className={`fixed top-0 right-0 bg-gray-700 shadow-2xl w-96 h-full grid grid-rows-[auto_1fr_auto]
-        transform transition-transform duration-500
-        ${statusTab === false ? "translate-x-full" : ""}
-        `}>
+        <div
+            className={`fixed top-0 right-0 bg-gray-700 shadow-2xl h-full grid grid-rows-[auto_1fr_auto]
+            transform transition-transform duration-500
+            w-full sm:w-96 md:w-128 lg:w-1/3 xl:w-1/4
+            ${statusTab === false ? 'translate-x-full' : ''}
+            `}
+        >
             <h2 className='p-5 text-white text-2xl'>Shopping Cart</h2>
             <div className='p-5 overflow-y-auto'>
-                {carts.map((item, key) =>
-                    <CartItem key={key} data={item}/>
-                )}
+                {carts.map((item, key) => (
+                    <CartItem key={key} data={item} />
+                ))}
             </div>
             <div className='p-5 bg-gray-800 text-white'>
                 <div className='mb-4'>
@@ -54,38 +59,47 @@ const CartTab = () => {
                     <p className='text-xl font-bold'>Total: {formatCurrency(finalPrice)}</p>
                 </div>
                 <div className='mb-4'>
-                    <div className='flex mb-2'>
-                        <input 
+                    <div className='flex flex-col sm:flex-row mb-2'>
+                        <input
                             type='number'
                             placeholder='Discount amount'
                             value={discountInput}
                             onChange={(e) => setDiscountInput(e.target.value)}
-                            className='p-2 text-black w-1/2'
+                            className='p-2 text-black w-full sm:w-1/2'
                         />
-                        <select 
+                        <select
                             value={discountType}
                             onChange={(e) => setDiscountType(e.target.value)}
-                            className='p-2 text-black w-1/4 ml-2'
+                            className='p-2 text-black w-full sm:w-1/4 mt-2 sm:mt-0 sm:ml-2'
                         >
-                            <option value="fixed">$</option>
-                            <option value="percentage">%</option>
+                            <option value='fixed'>$</option>
+                            <option value='percentage'>%</option>
                         </select>
-                        <button 
+                        <button
                             onClick={handleApplyDiscount}
-                            className='bg-blue-500 p-2 ml-2 w-1/4'
+                            className='bg-blue-500 p-2 mt-2 sm:mt-0 sm:ml-2 w-full sm:w-1/4'
                         >
                             Apply
                         </button>
                     </div>
-                   
                 </div>
                 <div className='grid grid-cols-2 gap-2'>
-                    <button className='bg-black text-white p-2' onClick={handleCloseTabCart}>CLOSE</button>
-                    <button className='bg-amber-600 text-white p-2' onClick={handleCheckout}>CHECKOUT</button>
+                    <button
+                        className='bg-black text-white p-2'
+                        onClick={handleCloseTabCart}
+                    >
+                        CLOSE
+                    </button>
+                    <button
+                        className='bg-amber-600 text-white p-2'
+                        onClick={handleCheckout}
+                    >
+                        CHECKOUT
+                    </button>
                 </div>
             </div>
         </div>
-    )
-}
+    );
+};
 
-export default CartTab
+export default CartTab;
